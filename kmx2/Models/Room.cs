f4df33c22500f4919ce9d282bf99e2152c3e9512
jollybean3 <jollybean3@outlook.com>
2025-02-02 20:04:45 -0500

    added first models

---
+++ kmx2/Models/Room.cs
@@ -0,0 +1,21 @@
+﻿namespace kmx2.Models
+{
+    public class Room
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Code { get; private set; }
+
+        public Room(string name, string description, string code)
+        {
+            Name = name;
+            Description = description;
+
+            if (code == null || code.Length == 0)
+                throw new ArgumentException("code is required");
+
+            Code = code;
+        }
+    }
+}
---
+++ kmx2/Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace kmx2.Models
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        public string Salt { get; private set; }
+        public List<Room>? Rooms { get; private set; }
+
+        public User(string userName, string password, string confirmPassword) 
+        {
+            if (userName.Length < 5)
+                throw new Exception("UserName must be at least 5 characters long.");
+
+            if (password.Length < 4)
+                throw new Exception("Password must be at least 4 characters long.");
+
+            if (password != confirmPassword)
+                throw new Exception("Passwords do not match");
+
+            UserName = userName;
+            SetPassword(password);
+        }
+
+        public void SetPassword(string password)
+        {
+            Salt = password;
+            Password = password;
+        }
+    }
+}
